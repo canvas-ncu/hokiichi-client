8c7891db9e8062b600ca6e5bfce572861c4c5956
sawa-zen <sawadasuiren@gmail.com>
2017-06-24 19:50:21 +0900

    Add App.js

--- src/js/AppBootstrap.js
+++ src/js/AppBootstrap.js
@@ -1,1 +1,9 @@
-console.info('test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// Reactを描画
+ReactDOM.render(
+  <App />,
+  document.getElementById('main')
+);
